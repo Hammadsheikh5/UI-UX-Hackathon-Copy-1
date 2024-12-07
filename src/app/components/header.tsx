22c563b88e136f6741f277b9292bca4495cf5017
Hammad <mhammadkhi5@gmail.com>
2024-12-08 00:07:17 +0500

    Add li

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -10,6 +10,7 @@ export default function Header() {
           <li>About</li>
           <li>Services</li>
           <li>Contact</li>
+          <li>Projects</li>
         </ul>
       </div>
     </div>
